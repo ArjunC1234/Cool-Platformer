eefaf9d7890e73bb7d34e2c5721e7357800c112b
Glitch (empty-welcome) <none>
2021-12-01 02:09:20 +0000

    ❤️👜 Checkpoint ./chat.html:13138733/332 ./server.js:13138733/220

--- server.js
+++ server.js
@@ -22,10 +22,14 @@ app.get('/chat/*', (req, res) => {
 io.on('connection', (socket) => {
   io.emit('connection')
   socket.on('chat message', msg => {
+    if(msg.match(/\\name\s(.*)/gims)){
+      usid = msg.replace(/\\name\s(.*)/gims, '$1');
+      console.log(msg.replace(/\\name\s(.*)/gims, '$1'))
+    }
     io.emit('chat message', usid + ': ' + msg);
   });
 });
 
 http.listen(port, () => {
-  console.log(`running at http://localhost:${port}/`);
+  console.log(`running at ${port}`);
 });
