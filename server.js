8ff484946ea9d82e952b220c37233280feae4049
Glitch (empty-welcome) <none>
2021-12-01 01:46:18 +0000

    👩‍👩‍👦🐢 Checkpoint ./server.js:13138733/1032 ./index.html:13138733/240

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const net = require('net');
+const client = net.connect({port: 80, host:"google.com"}, () => {
+  v
+});
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/username', (req, res) => {
-  res.sendFile(__dirname + '/username.html');
-});
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+    io.emit('chat message', '' + msg);
   });
 });
 
