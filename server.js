7ff94bc2f24ae53cd56d35fc2d973f220ccae1cb
Glitch (empty-welcome) <none>
2022-10-08 01:53:22 +0000

    🏔🐒 Checkpoint ./server.js:31523611/636 ./index.html:31523611/761

--- server.js
+++ server.js
@@ -8,28 +8,18 @@ app.set("trust proxy", true);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-function generate(n) {
-  var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
 
-  if ( n > max ) {
-          return generate(max) + generate(n - max);
-  }
-
-  max        = Math.pow(10, n+add);
-  var min    = max/10; // Math.pow(10, n) basically
-  var number = Math.floor( Math.random() * (max - min + 1) ) + min;
-
-  return ("" + number).substring(add); 
-}
 io.on('connection', (socket) => {
-  io.sockets.emit('connected', )
-  socket.on('disconnect', () => {
-    io.sockets.emit('disconnected')
+  socket.on('dc', (id) => {
+    io.sockets.emit('disconnected', id)
   });
   
   socket.on('chat message', (name, msg, id) => {
     io.sockets.emit('chat message', name, msg, id)
   });
+  socket.on('connect', (id) >= {
+    console.log(id)
+  })
 });
 
 
