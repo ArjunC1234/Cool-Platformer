58e16de0622e38b779a7e8d26c7e39fb33b000fd
Glitch (cool-platformer) <none>
2022-11-02 17:01:05 +0000

    😎🌮 Checkpoint ./server.js:31523611/101

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const sqlite3 = require('sqlite3').verbose();
 
 app.set("trust proxy", true);
 
-var down = false
-var allowedips = [process.env.ALLOWED1]
+var down = true
+var allowedips = [process.env.ALLOWED1, process.env.ALLOWED2, process.env.ALLOWED3]
 
 var levelSockets = []
 function checkIP (req, res, path) {
